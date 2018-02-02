839062a6c6eb8b051633a613ae6fab46cdd5af80
skyworth <skyworth@skyworthdeiMac.local>
2018-02-02 10:11:13 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <home></home>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Home from './components/home/home'
+
 export default {
-  name: 'App'
+  components:Home
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld／HelloWorld.vue'
+import Home from '@/components/home／home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      // path: '/',
-      // component: HelloWorld
+      path: '/',
+      component: Home
     }
   ]
 })
